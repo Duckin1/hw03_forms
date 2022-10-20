d3f724824bc036b23bf639468fb0c3c4c07a6240
Миннибаев Алмаз Айдарович <tat100malai@tattelecom.ttc>
2022-10-20 11:25:45 +0300

    review_final

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,7 @@
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
-from django.conf import settings
 
 from .forms import PostForm
 from .models import Group, Post, User
