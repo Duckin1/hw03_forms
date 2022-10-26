2cfa25624a2205e9a51e5e2cd7a127fb9026044f
Алмаз Миннибаев <almaz200@bk.ru>
2022-10-27 00:07:44 +0300

    test_views_no_context

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,1 +1,62 @@
-from django.test import TestCase
+from django import forms
+from django.shortcuts import get_object_or_404
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from posts.models import Group, Post, User
+
+
+class TaskURLTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        # Создаем неавторизованный клиент
+        cls.guest_client = Client()
+        # Создаем пользователя
+        cls.user = User.objects.create_user(username='HasNoName')
+        # Создаем второй клиент
+        cls.authorized_client = Client()
+        # Авторизуем пользователя
+        cls.authorized_client.force_login(cls.user)
+
+        cls.group = Group.objects.create(
+            title='Тестовая Группа',
+            slug='test-slug',
+            description='тестовое описание группы'
+        )
+        cls.post = Post.objects.create(
+            text='Тестовый текст',
+            author=cls.user,
+            group=cls.group,
+        )
+
+    # Проверяем используемые шаблоны
+    def test_pages_uses_correct_template(self):
+        """URL-адрес использует соответствующий шаблон."""
+        # Собираем в словарь пары "имя_html_шаблона: reverse(name)"
+        templates_pages_names = {
+            'posts/index.html': (reverse
+                ('posts:index')
+            ),
+            'posts/group_list.html': (reverse
+                ('posts:group_list', kwargs={'slug': 'test-slug'})
+            ),
+            'posts/profile.html': reverse(
+                'posts:profile',
+                args=[get_object_or_404(User, username='HasNoName')]
+            ),
+            'posts/post_detail.html': (reverse
+                ('posts:post_detail', kwargs={'post_id': '1'})
+            ),
+            'posts/create_post.html': reverse(
+                'posts:post_edit', kwargs={'post_id': '1'}
+            ),
+            'posts/create_post.html': reverse(
+                'posts:post_create'
+            ),
+        }
+        # Проверяем, что при обращении к name вызывается соответствующий HTML-шаблон
+        for template, reverse_name in templates_pages_names.items():
+            with self.subTest(reverse_name=reverse_name):
+                response = self.authorized_client.get(reverse_name)
+                self.assertTemplateUsed(response, template)
